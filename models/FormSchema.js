a63d17342e22e01467d773a2c90259f8a22e7193
garcia2202 <tonato.thiago@gmail.com>
2023-11-01 16:00:49 -0300

    funcionando

--- models/FormSchema.js
+++ models/FormSchema.js
@@ -1,39 +1,42 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const formSchema = new mongoose.Schema({
-  dataSolicitação: {
-    type: Date,
-    default: Date.now(),
-    required: true
-  },
-  nome: {
-    type: String,
-    required: true
-  },
-  membros: {
-    type: String,
-    required: true
-  },
-  dataEntrega: {
-    type: Date,
-    required: true
-  },
-  tecnologias: {
-    type: Array,
-    required: true
-  },
-  outraTec: {
-    type: String,
-    required: false
-  },
-  contextoUso: {
-    type: String,
-    required: false
-  },
-  acessoUsuario: {
-    type: String,
-    required: false
-  },
-});
+// const FormSchema = new mongoose.Schema({
+//   startDate: {
+//     type: Date,
+//     required: true,
+//   },
+//   nomePO: {
+//     type: String,
+//     required: true,
+//   },
+//   membros: {
+//     type: String,
+//     required: true,
+//   },
+//   endDate: {
+//     type: Date,
+//     required: true,
+//   },
+//   tecnologias: {
+//     type: Array,
+//     required: true,
+//   },
+//   outraTecCheck: {
+//     type: Boolean,
+//     required: false,
+//   },
+//   outraTec: {
+//     type: String,
+//     required: false,
+//   },
+//   contextoUso: {
+//     type: String,
+//     required: false,
+//   },
+//   acessoUsuario: {
+//     type: String,
+//     required: false,
+//   },
+// });
 
-export default mongoose.model('formModel', formSchema)
+// export default mongoose.model("formSchema", FormSchema);
